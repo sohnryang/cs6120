bbfa4b66923f154b7dd6e87afa8c8388a905c6f6
Ryang Sohn <loop.infinitely@gmail.com>
2024-08-09 13:58:57 +0900

    feat(bril_types): switch to simdjson
    I didn't know that simdjson does not support serialization

--- include/bril_types.hh
+++ include/bril_types.hh
@@ -6,6 +6,8 @@
 #include <variant>
 #include <vector>
 
+#include "nlohmann/json.hpp"
+
 struct ValueOp;
 struct EffectOp;
 struct Label;
@@ -15,9 +17,17 @@ struct Type {
   std::shared_ptr<Type> param_type;
 };
 
+void to_json(nlohmann::json &j, const Type &type);
+
+void from_json(const nlohmann::json &j, Type &type);
+
 struct Label {
   std::string name;
 };
+
+void to_json(nlohmann::json &j, const Label &l);
+
+void from_json(const nlohmann::json &j, Label &l);
 
 enum class ValueOpKind {
   Add,
@@ -44,6 +54,10 @@ struct ValueOp {
   Type type;
 };
 
+void to_json(nlohmann::json &j, const ValueOp &op);
+
+void from_json(const nlohmann::json &j, ValueOp &op);
+
 enum class EffectOpKind {
   Jmp,
   Br,
@@ -59,10 +73,15 @@ struct EffectOp {
   std::vector<std::string> labels;
 };
 
+void to_json(nlohmann::json &j, const EffectOp &op);
+
+void from_json(const nlohmann::json &j, EffectOp &op);
+
 struct Constant {
   std::variant<int64_t, bool> value;
+  std::string dest;
 
-  inline Type type() {
+  inline Type type() const {
     if (value.index() == 0)
       return {.name = "int", .param_type = nullptr};
     else
@@ -70,14 +89,30 @@ struct Constant {
   };
 };
 
+void to_json(nlohmann::json &j, const Constant &c);
+
+void from_json(const nlohmann::json &j, Constant &c);
+
 using Op = std::variant<ValueOp, EffectOp, Constant>;
+
+void to_json(nlohmann::json &j, const Op &op);
+
+void from_json(const nlohmann::json &j, Op &op);
 
 struct Argument {
   std::string name;
   Type type;
 };
 
+void to_json(nlohmann::json &j, const Argument &arg);
+
+void from_json(const nlohmann::json &j, Argument &arg);
+
 using Instruction = std::variant<Op, Label>;
+
+void to_json(nlohmann::json &j, const Instruction &instr);
+
+void from_json(const nlohmann::json &j, Instruction &instr);
 
 struct Function {
   std::string name;
@@ -86,6 +121,14 @@ struct Function {
   std::vector<Instruction> instrs;
 };
 
+void to_json(nlohmann::json &j, const Function &func);
+
+void from_json(const nlohmann::json &j, Function &func);
+
 struct Program {
   std::vector<Function> functions;
 };
+
+void to_json(nlohmann::json &j, const Program &prog);
+
+void from_json(const nlohmann::json &j, Program &prog);
---
+++ src/bril_types.cc
@@ -0,0 +1,191 @@
+#include "bril_types.hh"
+
+#include "nlohmann/json.hpp"
+
+#include "fmt/format.h"
+
+using nlohmann::json;
+
+void to_json(json &j, const Type &t) {
+  if (t.param_type) {
+    j = json{{t.name, *t.param_type}};
+  } else {
+    j = t.name;
+  }
+}
+
+void from_json(const json &j, Type &t) {
+  if (j.is_string()) {
+    t.name = j.get<std::string>();
+    t.param_type = nullptr;
+  } else if (j.is_object()) {
+    auto it = j.begin();
+    t.name = it.key();
+    t.param_type = std::make_shared<Type>();
+    from_json(it.value(), *t.param_type);
+  } else
+    throw std::runtime_error("Invalid type format");
+}
+
+void to_json(json &j, const Label &l) { j["label"] = l.name; }
+
+void from_json(const json &j, Label &l) {
+  l.name = j["label"].get<std::string>();
+}
+
+void to_json(json &j, const ValueOp &op) {
+  static const std::unordered_map<ValueOpKind, std::string> op_map = {
+      {ValueOpKind::Add, "add"},  {ValueOpKind::Mul, "mul"},
+      {ValueOpKind::Sub, "sub"},  {ValueOpKind::Div, "div"},
+      {ValueOpKind::Eq, "eq"},    {ValueOpKind::Lt, "lt"},
+      {ValueOpKind::Gt, "gt"},    {ValueOpKind::Le, "le"},
+      {ValueOpKind::Ge, "ge"},    {ValueOpKind::Not, "not"},
+      {ValueOpKind::And, "and"},  {ValueOpKind::Or, "or"},
+      {ValueOpKind::Call, "call"}};
+
+  j["op"] = op_map.at(op.kind);
+  j["dest"] = op.dest;
+  j["type"] = op.type;
+  if (!op.args.empty())
+    j["args"] = op.args;
+  if (!op.funcs.empty())
+    j["funcs"] = op.funcs;
+  if (!op.labels.empty())
+    j["labels"] = op.labels;
+}
+
+void from_json(const json &j, ValueOp &op) {
+  static const std::unordered_map<std::string, ValueOpKind> op_map = {
+      {"add", ValueOpKind::Add},  {"mul", ValueOpKind::Mul},
+      {"sub", ValueOpKind::Sub},  {"div", ValueOpKind::Div},
+      {"eq", ValueOpKind::Eq},    {"lt", ValueOpKind::Lt},
+      {"gt", ValueOpKind::Gt},    {"le", ValueOpKind::Le},
+      {"ge", ValueOpKind::Ge},    {"not", ValueOpKind::Not},
+      {"and", ValueOpKind::And},  {"or", ValueOpKind::Or},
+      {"call", ValueOpKind::Call}};
+
+  op.kind = op_map.at(j["op"].get<std::string>());
+  op.dest = j["dest"].get<std::string>();
+  j["type"].get_to(op.type);
+  if (j.contains("args"))
+    j["args"].get_to(op.args);
+  if (j.contains("funcs"))
+    j["funcs"].get_to(op.funcs);
+  if (j.contains("labels"))
+    j["labels"].get_to(op.labels);
+}
+
+void to_json(json &j, const EffectOp &op) {
+  static const std::unordered_map<EffectOpKind, std::string> op_map = {
+      {EffectOpKind::Jmp, "jmp"},
+      {EffectOpKind::Br, "br"},
+      {EffectOpKind::Call, "call"},
+      {EffectOpKind::Ret, "ret"},
+      {EffectOpKind::Print, "print"}};
+
+  j["op"] = op_map.at(op.kind);
+  if (!op.args.empty())
+    j["args"] = op.args;
+  if (!op.funcs.empty())
+    j["funcs"] = op.funcs;
+  if (!op.labels.empty())
+    j["labels"] = op.labels;
+}
+
+void from_json(const json &j, EffectOp &op) {
+  static const std::unordered_map<std::string, EffectOpKind> op_map = {
+      {"jmp", EffectOpKind::Jmp},
+      {"br", EffectOpKind::Br},
+      {"call", EffectOpKind::Call},
+      {"ret", EffectOpKind::Ret},
+      {"print", EffectOpKind::Print}};
+
+  op.kind = op_map.at(j["op"].get<std::string>());
+  if (j.contains("args"))
+    j["args"].get_to(op.args);
+  if (j.contains("funcs"))
+    j["funcs"].get_to(op.funcs);
+  if (j.contains("labels"))
+    j["labels"].get_to(op.labels);
+}
+
+void to_json(nlohmann::json &j, const Constant &c) {
+  j["op"] = "const";
+  j["type"] = c.type().name;
+  j["dest"] = c.dest;
+  std::visit([&j](auto &&arg) { j["value"] = arg; }, c.value);
+}
+
+void from_json(const nlohmann::json &j, Constant &c) {
+  j["dest"].get_to(c.dest);
+  auto type_name = j["type"];
+  if (type_name == "int")
+    c.value = j["value"].get<int64_t>();
+  else if (type_name == "bool")
+    c.value = j["value"].get<bool>();
+  else
+    throw std::runtime_error(
+        fmt::format("Invalid constant type: {}", std::string(type_name)));
+}
+
+void to_json(json &j, const Op &op) {
+  std::visit([&j](auto &&arg) { to_json(j, arg); }, op);
+}
+
+void from_json(const json &j, Op &op) {
+  if (j["op"] == "const") {
+    op = j.get<Constant>();
+  } else if (j.contains("dest")) {
+    op = j.get<ValueOp>();
+  } else {
+    op = j.get<EffectOp>();
+  }
+}
+
+void to_json(json &j, const Argument &arg) {
+  j = json{{"name", arg.name}, {"type", arg.type}};
+}
+
+void from_json(const json &j, Argument &arg) {
+  j["name"].get_to(arg.name);
+  j["type"].get_to(arg.type);
+}
+
+void to_json(json &j, const Instruction &instr) {
+  std::visit([&j](auto &&arg) { to_json(j, arg); }, instr);
+}
+
+void from_json(const json &j, Instruction &instr) {
+  if (j.contains("label")) {
+    instr = j.get<Label>();
+  } else {
+    instr = j.get<Op>();
+  }
+}
+
+void to_json(json &j, const Function &func) {
+  j["name"] = func.name;
+  if (!func.args.empty())
+    j["args"] = func.args;
+  if (func.return_type)
+    j["type"] = *func.return_type;
+  j["instrs"] = func.instrs;
+}
+
+void from_json(const json &j, Function &func) {
+  j["name"].get_to(func.name);
+  if (j.contains("args"))
+    j["args"].get_to(func.args);
+  if (j.contains("type")) {
+    func.return_type = j["type"].get<Type>();
+  } else {
+    func.return_type = std::nullopt;
+  }
+  j["instrs"].get_to(func.instrs);
+}
+
+void to_json(json &j, const Program &prog) { j["functions"] = prog.functions; }
+
+void from_json(const json &j, Program &prog) {
+  j["functions"].get_to(prog.functions);
+}
