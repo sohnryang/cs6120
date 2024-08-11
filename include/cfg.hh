109fa7abd03d725c2d00f9f414744f59a13b8418
Ryang Sohn <loop.infinitely@gmail.com>
2024-08-11 17:46:58 +0900

    feat(analysis): add equality operator overload for CFG

--- include/cfg.hh
+++ include/cfg.hh
@@ -12,12 +12,16 @@ struct BasicBlock {
   std::string name;
   std::vector<Op> instrs;
   std::optional<EffectOp> terminator;
+
+  bool operator==(const BasicBlock &) const = default;
 };
 
 struct ControlFlowGraph {
   std::vector<Argument> args;
   std::vector<BasicBlock> blocks;
   std::unordered_map<std::size_t, std::vector<std::size_t>> successors;
+
+  bool operator==(const ControlFlowGraph &) const = default;
 
   static ControlFlowGraph from_function(const Function &function);
 
