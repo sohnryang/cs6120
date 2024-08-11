3609ad19b4b766a75b71d52aa02fb953fe90a39d
Ryang Sohn <loop.infinitely@gmail.com>
2024-08-11 18:28:08 +0900

    feat(utils): add utility functions and macro for hashable types
    It's good enough (TM)

--- include/utils.hh
+++ include/utils.hh
@@ -1,7 +1,30 @@
 #pragma once
 
+#include <cstddef>
+#include <functional>
+
 template <class... Ts> struct match : Ts... {
   using Ts::operator()...;
 };
 
 template <class... Ts> match(Ts...) -> match<Ts...>;
+
+inline void hash_combine(std::size_t &seed) {}
+
+template <class T, class... Ts>
+inline void hash_combine(std::size_t &seed, const T &v, Ts... rest) {
+  std::hash<T> hasher;
+  seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  hash_combine(seed, rest...);
+}
+
+#define MAKE_HASHABLE(type, ...)                                               \
+  namespace std {                                                              \
+  template <> struct hash<type> {                                              \
+    std::size_t operator()(const type &t) const {                              \
+      std::size_t ret = 0;                                                     \
+      hash_combine(ret, __VA_ARGS__);                                          \
+      return ret;                                                              \
+    }                                                                          \
+  };                                                                           \
+  }
