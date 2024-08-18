2630ba6dd8cfc79a43449952c59d8ffc3c735a8f
Ryang Sohn <loop.infinitely@gmail.com>
2024-08-18 18:33:36 +0900

    refactor(analysis): add new pass variant, `AnalysisPass`

--- include/pass.hh
+++ include/pass.hh
@@ -1,39 +1,64 @@
 #pragma once
 
+#include "utils.hh"
+
 #include <memory>
+#include <utility>
+#include <variant>
 #include <vector>
+
+#include "nlohmann/json.hpp"
 
 template <class T> class TransformPass {
 public:
   virtual T operator()(const T &) const = 0;
 };
 
-template <class T> class TransformPassRunner {
-private:
-  std::vector<std::shared_ptr<TransformPass<T>>> _passes;
-
+template <class T> class AnalysisPass {
 public:
-  TransformPassRunner() = default;
-  TransformPassRunner(const TransformPassRunner &) = default;
-  TransformPassRunner(TransformPassRunner &&) = default;
-  TransformPassRunner &operator=(const TransformPassRunner &) = default;
-  TransformPassRunner &operator=(TransformPassRunner &&) = default;
+  virtual void operator()(const T &) = 0;
 
-  TransformPassRunner(
-      const std::vector<std::shared_ptr<TransformPass<T>>> &passes);
-
-  T run_passes(const T &cfg) const;
+  virtual nlohmann::json serialized_result() = 0;
 };
 
 template <class T>
-TransformPassRunner<T>::TransformPassRunner(
-    const std::vector<std::shared_ptr<TransformPass<T>>> &passes)
+using Pass = std::variant<std::shared_ptr<TransformPass<T>>,
+                          std::shared_ptr<AnalysisPass<T>>>;
+
+template <class T> class PassRunner {
+private:
+  std::vector<Pass<T>> _passes;
+
+public:
+  PassRunner() = default;
+  PassRunner(const PassRunner &) = default;
+  PassRunner(PassRunner &&) = default;
+  PassRunner &operator=(const PassRunner &) = default;
+  PassRunner &operator=(PassRunner &&) = default;
+
+  PassRunner(const std::vector<Pass<T>> &passes);
+
+  std::pair<T, nlohmann::json> operator()(const T &pass_input);
+};
+
+template <class T>
+PassRunner<T>::PassRunner(const std::vector<Pass<T>> &passes)
     : _passes(passes) {}
 
 template <class T>
-T TransformPassRunner<T>::run_passes(const T &pass_input) const {
+std::pair<T, nlohmann::json> PassRunner<T>::operator()(const T &pass_input) {
+  std::vector<nlohmann::json> results;
   auto transformed = pass_input;
-  for (const auto &pass : _passes)
-    transformed = (*pass)(transformed);
-  return transformed;
+  for (const auto pass : _passes)
+    std::visit(match{
+                   [&](std::shared_ptr<TransformPass<T>> transform_pass) {
+                     transformed = (*transform_pass)(transformed);
+                   },
+                   [&](std::shared_ptr<AnalysisPass<T>> analysis_pass) {
+                     (*analysis_pass)(transformed);
+                     results.push_back(analysis_pass->serialized_result());
+                   },
+               },
+               pass);
+  return {transformed, results};
 }
--- src/opt.cc
+++ src/opt.cc
@@ -5,6 +5,7 @@
 
 #include <cstdlib>
 #include <exception>
+#include <fstream>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -18,16 +19,19 @@
 
 using json = nlohmann::json;
 
-static const std::unordered_map<
-    std::string, std::shared_ptr<TransformPass<ControlFlowGraph>>>
-    PASSES = {
+static const std::unordered_map<std::string, Pass<ControlFlowGraph>>
+    CFG_PASSES = {
         {"local_dce", std::make_shared<LocalDeadCodeElimination>()},
 };
 
 int main(int argc, char *argv[]) {
   argparse::ArgumentParser parser("opt");
-  std::vector<std::string> pass_names;
-  parser.add_argument("--pass").append().store_into(pass_names);
+  std::vector<std::string> cfg_pass_names;
+  std::string analysis_out_filename;
+  parser.add_argument("--cfg-pass").append().store_into(cfg_pass_names);
+  parser.add_argument("--analysis-out")
+      .default_value("analysis_out.json")
+      .store_into(analysis_out_filename);
 
   try {
     parser.parse_args(argc, argv);
@@ -36,27 +40,32 @@ int main(int argc, char *argv[]) {
     std::exit(1);
   }
 
-  std::vector<std::shared_ptr<TransformPass<ControlFlowGraph>>> passes;
-  for (const auto &pass_name : pass_names)
-    passes.push_back(PASSES.at(pass_name));
-  TransformPassRunner runner(passes);
+  std::vector<Pass<ControlFlowGraph>> cfg_passes;
+  for (const auto &pass_name : cfg_pass_names)
+    cfg_passes.push_back(CFG_PASSES.at(pass_name));
+  PassRunner<ControlFlowGraph> cfg_pass_runner(cfg_passes);
 
   json j;
   std::cin >> j;
   Program program(j);
 
   Program optimized_program;
+  json analysis_out;
   for (const auto &function : program.functions) {
-    const auto optimized =
-        runner.run_passes(ControlFlowGraph::from_function(function));
+    const auto [optimized, serialized_results] =
+        cfg_pass_runner(ControlFlowGraph::from_function(function));
     optimized_program.functions.push_back({
         .name = function.name,
         .args = function.args,
         .return_type = function.return_type,
         .instrs = optimized.into_instrs(),
     });
+    analysis_out["cfg"][function.name] = serialized_results;
   }
 
   json serialized = optimized_program;
   fmt::println(std::cout, "{}", fmt::streamed(serialized));
+
+  std::ofstream outfile(analysis_out_filename);
+  outfile << analysis_out;
 }
